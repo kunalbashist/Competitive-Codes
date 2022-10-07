95c1534db6ad76b912b6b409b210f542e54122a0
Kunal Bashist <62015640+kunalbashist@users.noreply.github.com>
2022-10-07 15:15:37 +0530

    reverse a string
    WAP to reverse a string

---
+++ C++/reverse.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin,s);
+    string k=s;
+    reverse(k.begin(),k.end());
+    cout<<k<<endl;
+    return 0;
+}
